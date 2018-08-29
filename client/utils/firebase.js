93299f6015e0233d9568f1b7e46828aa54d20d89
r0b1n1sl4m <r0b1n1sl4m@gmail.com>
2018-08-29 15:24:18 +0600

    Updated dependency

--- client/utils/firebase.js
+++ client/utils/firebase.js
@@ -1,18 +1,10 @@
+import { firebaseConfig } from './configs';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
 
-const config = {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
-};
-
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const database = firebase.database();
